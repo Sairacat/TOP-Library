02162a25b1ab6b85bdc6005cdecd014a7954420f
Sairacat <201769jzy@gmail.com>
2026-05-12 16:56:31 +0800

    Completed the basic logic of adding books to shelf Still need to work on that logic a little bit.

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ const author = document.querySelector('#author');
 const pages = document.querySelector('#pages')
 const isRead = document.querySelector('#status')
 const submitBtn = document.querySelector('.submit-btn')
+const bookShelf = document.querySelector('.bookshelf')
+const bookSheet = document.querySelector('.book-sheet')
 const myLibrary = [];
+
+bookSheet.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    displayBook();
+})
 
 
 function Book (title, author, pages, isRead, id) {
@@ -19,16 +27,64 @@ function Book (title, author, pages, isRead, id) {
 
 
 function addBookToLibrary () {
-    const bookTitle = title.value;
-    const bookAuthor = author.value;
-    const bookPages = pages.value;
-    const bookStatus = isRead.checked;
-    const bookId = Math.random().toString(36).slice(2);
+    const newBookTitle = title.value;
+    const newBookAuthor = author.value;
+    const newBookPages = pages.value;
+    const newBookStatus = isRead.checked;
+    const newBookId = Math.random().toString(36).slice(2);
 
-    const newBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus, bookId)
-    myLibrary.push(newBook)
+    const newBook = new Book(newBookTitle, newBookAuthor, newBookPages, newBookStatus, newBookId);
+    myLibrary.push(newBook);
 }
 
 function displayBook () {
-    
+    for(let book of myLibrary) {
+        
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const bookTitle = document.createElement('div');
+        bookTitle.textContent = book.title;
+        bookTitle.classList.add('booktitle');
+        
+        const bookAuthor = document.createElement('div');
+        bookAuthor.textContent = `By ${book.author}`;
+        bookAuthor.classList.add('bookauthor');
+
+        const bookPages = document.createElement('div');
+        bookPages.textContent = book.pages;
+        bookPages.classList.add('bookpages');
+
+        const bookStatus = document.createElement('input');
+        bookStatus.type = 'checkbox';
+        bookStatus.checked = book.isRead;
+        bookStatus.classList.add('bookstatus');
+
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Remove';
+        removeBtn.id = book.id;
+        removeBtn.classList.add('remove-btn');
+        removeBtn.addEventListener('click', () => {
+            let cards = document.querySelectorAll('.card');
+            for(let book of myLibrary) {
+                if(removeBtn.id === book.id) {
+                    let index = myLibrary.indexOf(book);
+                    myLibrary.splice(index, 1);
+                }
+            }
+            for(let card of cards) {
+                if(card.contains(removeBtn)) {
+                    bookShelf.removeChild(card)
+                }
+            }
+        })
+
+        card.appendChild(bookTitle);
+        card.appendChild(bookAuthor);
+        card.appendChild(bookPages);
+        card.appendChild(bookStatus);
+        card.appendChild(removeBtn);
+        bookShelf.appendChild(card);
+
+    }
 }
