bfc99305db061928f4a2189b462ad7f6d58050ad
Sairacat <201769jzy@gmail.com>
2026-05-12 14:14:46 +0800

    Compltetd the basic layout of bookshelf page. Add a webfont to the local page.

--- script.js
+++ script.js
@@ -9,14 +9,26 @@ const submitBtn = document.querySelector('.submit-btn')
 const myLibrary = [];
 
 
-function Book (title, author, pages, isRead) {
+function Book (title, author, pages, isRead, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.id = id
 }
 
 
 function addBookToLibrary () {
+    const bookTitle = title.value;
+    const bookAuthor = author.value;
+    const bookPages = pages.value;
+    const bookStatus = isRead.checked;
+    const bookId = Math.random().toString(36).slice(2);
 
+    const newBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus, bookId)
+    myLibrary.push(newBook)
+}
+
+function displayBook () {
+    
 }
